28e5479adca80098c5906bcc3fb910e68f247994
Jimcrzz <style503us@gmail.com>
2025-03-04 11:18:17 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    showSection('income-expense');
+});
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    const activeSection = document.getElementById(sectionId);
+    if (activeSection) {
+        activeSection.classList.add('active');
+    }
+}
